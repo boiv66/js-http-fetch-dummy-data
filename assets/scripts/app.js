9cc1ad82014bb75636a1df8404ee13816c809260
boiv66 <vothanhboiloc2409@gmail.com>
2022-05-24 20:16:54 +0700

    handling error when sending http request

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,33 @@
 // get html element
 const listEl = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const addBtn = document.querySelector('#new-post button');
+const fetchBtn = document.querySelector('#available-posts button'); 
+const form = document.querySelector('#new-post form');
+const postList = document.querySelector('ul');
+
 
 function sendHttpRequest(method, url, data) {
+    
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
 
     xhr.onload = function () {
-        resolve(JSON.parse(xhr.response)); 
+        if (xhr.status >= 200 && xhr.status < 300){
+            resolve(JSON.parse(xhr.response)); 
+        }
+        else{
+            reject(new Error('Something went wrong!'))
+        }
+
+        
       
     };
+
+    xhr.onerror = function() {
+       reject(new Error("Fail to send request"))
+    }
 
     xhr.send(JSON.stringify(data));
   });
@@ -23,16 +40,17 @@ async function fetchPost(){
       
     console.log(listOfPost);
     for (const post of listOfPost) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    listEl.append(postEl);
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector("h2").textContent = post.title.toUpperCase();
+        postEl.querySelector("p").textContent = post.body;
+        postEl.querySelector("li").id = post.id; 
+        listEl.append(postEl);
     }
 
 }
 
 async function createPost(title, content){
-    const userId = Math.random()*1000;
+    const userId = parseInt(Math.random()*1000);
     const post = {
         title: title, 
         body: content, 
@@ -42,5 +60,24 @@ async function createPost(title, content){
 
 }
 
-fetchPost();
-createPost('Dummy', 'there is nothing here'); 
+fetchBtn.addEventListener('click', fetchPost); 
+form.addEventListener('submit', event => {
+    event.preventDefault(); 
+    const inputTitle = event.currentTarget.querySelector('#title').value; 
+    const inputContent = event.currentTarget.querySelector('#content').value;
+
+    createPost(inputTitle, inputContent); 
+
+})
+
+postList.addEventListener('click', event => {
+    if(event.target.tagName === 'BUTTON'){
+        console.log('clicked btn !');
+        const postId = event.target.closest('li').id;
+        console.log(`https://jsonplaceholder.typicode.com/posts/${postId}`); 
+        sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`, null)
+    }
+} )
+
+
+// createPost('Dummy', 'there is nothing here'); 
